3898ca9b3f0effce8ac7d42c6f8283a89cf77d4f
meet panchal <panchalmeet316@gmail.com>
2025-11-16 01:13:29 +0530

    added the todo form

--- frontend/app.py
+++ frontend/app.py
@@ -10,11 +10,20 @@ def home():
     time=datetime.datetime.now().strftime("%H:%M:%S")
     return render_template('index.html', day_of_week=day_of_week, time=time)
 
+@app.route('/todo')
+def todo():
+    return render_template('todo.html')
+
 @app.route('/submit',methods=['POST'])
 def submit():
     data = dict(request.form)
     requests.post(f"{BACKEND_URL}/submit", json=data)
     return "data submitted successfully"
 
+@app.route('/submittodoitem',methods=['POST'])
+def submittodoitem():
+    data = dict(request.form)
+    requests.post(f"{BACKEND_URL}/submittodoitem", json=data)
+    return "todo item submitted successfully"
 if __name__ == '__main__':
     app.run(port=9000,debug=True)
